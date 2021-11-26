d769f63744f346f3afe28f60584de4cc07ed8ad8
Mark Huang <hhzeos@gmail.com>
2021-11-26 22:59:13 +0800

    Update process_textual.py

--- preprocess/process_textual.py
+++ preprocess/process_textual.py
@@ -10,19 +10,25 @@ import torchtext as text
 from torchtext.data.utils import get_tokenizer
 from torchtext.datasets import IMDB
 from torchtext.vocab import vocab as Vocab
+
 from utils import load_text_data
 
 
-def encode(seq_tokens, token_to_idx):
+def encode(tokens: List[str], token_to_idx) -> List[int]:
     seq_idx = []
-    for token in seq_tokens:
+    for token in tokens:
         seq_idx.append(token_to_idx[token])
     return seq_idx
 
 
-def decode(seq_idx, idx_to_token, delim=None, stop_at_end=True):
+def decode(
+    encoded_sequence: List[int],
+    idx_to_token: dict,
+    delim: str = None,
+    stop_at_end: bool = True,
+) -> List[str]:
     tokens = []
-    for idx in seq_idx:
+    for idx in encoded_sequence:
         tokens.append(idx_to_token[idx])
         if stop_at_end and tokens[-1] == "<END>":
             break
@@ -32,21 +38,53 @@ def decode(seq_idx, idx_to_token, delim=None, stop_at_end=True):
         return delim.join(tokens)
 
 
-def encode_and_save(
-    args, vocab, tokenizer: Callable, texts: List[str], output: str = "encoded_text.pb"
-):
-    ### TODO: this whole function require testing
+def process_text_and_save(args, train=True):
 
+    EMBEDDING_DIM = 300
+    VOCAB_SIZE = 20000
+
+    # Load texts data from NFT json labels
+    ids, texts = load_text_data()
+
+    # Load English tokenizer
+    tokenizer: Callable = get_tokenizer("spacy", language="en")
+
+    ### Load vocab from disk for testing or build vocab for training
+    if not train:
+        # Load vocab from disk
+        # TODO
+        ...
+    else:
+        # Build the vocab
+        counter = Counter()
+        for i, line in enumerate(texts):
+            counter.update(tokenizer(line))
+
+        if len(counter) > VOCAB_SIZE:
+            ordered_dict = OrderedDict(counter.most_common()[:VOCAB_SIZE])
+        else:
+            ordered_dict = OrderedDict(counter.most_common())
+
+        vocab = Vocab(ordered_dict)
+
+        # insert special tokens and set default index to 'unknown'
+        vocab.insert_token("<PAD>", 0)
+        vocab.insert_token("<UNK>", 1)
+
+        # default index to return when out of vocab, idx 1 is leading to <UNK>
+        vocab.set_default_index(1)
+
+    # encode texts using vocab
     texts_encoded: List[List[int]] = []
     texts_lens: List[int] = []
     max_len: int = 0
 
     # encode
-    for i, text in enumerate(texts):
+    for i, t in enumerate(texts):
         # tokenize the text
-        tokens: List[str] = tokenizer(text)
+        tokens: List[str] = tokenizer(t)
         # encode text
-        text_encoded: List[int] = encode(tokens, vocab)
+        text_encoded: List[int] = encode(tokens=tokens, token_to_idx=vocab)
         texts_encoded.append(text_encoded)
 
         encoded_len: int = len(text_encoded)
@@ -57,63 +95,32 @@ def encode_and_save(
     for encoded in texts_encoded:
         encoded.extend([0] * (max_len - len(encoded)))
 
-    # save encoded texts
+    # convert lists to numpy array
     texts_encoded: np.ndarray = np.asarray(texts_encoded, dtype=np.int32)
     texts_lens: np.ndarray = np.asarray(texts_lens, dtype=np.int32)
 
+    if train:
+        # Load the pretrained GloVe embeddings
+        vec = text.vocab.GloVe(name="840B", dim=EMBEDDING_DIM)
+        # get all tokens from vocab
+        all_tokens: List[str] = vocab.get_itos()  # len = num_words
+        # get embeddings for all tokens
+        embedding_matrix = vec.get_vecs_by_tokens(
+            all_tokens
+        )  # (num_words, embedding_dim)
+    else:
+        # load from disk later
+        embedding_matrix = None
+
     obj = {
         "texts_encoded": texts_encoded,
-        "texts_lens": texts_lens,
+        "texts_lengths": texts_lens,
+        "embedding_matrix": embedding_matrix.numpy(),
     }
 
-    with open(output, "wb") as f:
+    with open("encoded_text.pb", "wb") as f:
         pickle.dump(obj, f)
 
 
-def process_text_and_save(args, train=True):
-    ### TODO: this whole function require testing
-
-    EMBEDDING_DIM = 300
-    VOCAB_SIZE = 20000
-
-    # Load texts data from NFT json labels
-    ids, texts = load_text_data()
-
-    # Load English tokenizer, tagger, parser and NER
-    tokenizer = get_tokenizer("spacy", language="en")
-
-    if not train:
-        ### Load vocab from disk
-        # TODO
-        ...
-    else:
-        ### Build vocab for training
-
-        # build the vocab
-        counter = Counter()
-        for i, line in enumerate(texts):
-            counter.update(tokenizer(line))
-
-        if len(counter) > VOCAB_SIZE:
-            ordered_dict = OrderedDict(counter.most_common()[:VOCAB_SIZE])
-        else:
-            ordered_dict = OrderedDict(counter.most_common())
-
-        # Build the vocab
-        vocab = Vocab(ordered_dict)
-
-        # insert special tokens and set default index to 'unknown'
-        vocab.insert_token("<PAD>", 0)
-        vocab.insert_token("<UNK>", 1)
-
-        # default index to return when out of vocab, idx 1 is leading to <UNK>
-        vocab.set_default_index(1)
-
-    ### TODO: the logic here is not done yet
-    ### TODO: call encode_and_save or merge encode_and_save
-
-    # # Load 300-dim GloVe embeddings
-    # vec = text.vocab.GloVe(name="840B", dim=300)
-    # # create the embedding matrix, a torch tensor in the shape (num_words+1, embedding_dim)
-    # word_emb = vec.get_vecs_by_tokens(vocab.get_itos())
-    # print(word_emb.shape)  # words -> vector
+if __name__ == "__main__":
+    process_text_and_save(args=None)
