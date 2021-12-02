b2b17df885a1e80752b0386a25ddd9c7847ae410
Mark Huang <hhzeos@gmail.com>
2021-12-03 02:33:51 +0800

    Update experiments

--- README.md
+++ README.md
@@ -15,11 +15,28 @@
 ## Feature Extraction
 
 -   Text
-    -   Bert
-    -   GloVe
+    -   GloVe-300
 -   Image
     -   Resnet
 -   Image Sequence
-    -   Resnext
+    -   Resnet18
 -   Audio
     -   TODO
+
+## Training Result
+
+| Experiment  | Test Accuracy | Remark             | Best Epoch |
+| ----------- | ------------- | ------------------ | ---------- |
+| `exp18`     | 0.4443        | freeze glove, B=16 | 9          |
+| `exp18-64`  | 0.4495        | freeze glove, B=64 | 13         |
+| `exp18-64L` | 0.4452        | train glove, B=64  | 9          |
+| `exp34`     | 0.4443        | freeze glove, B=16 | 8          |
+| `exp34`     | 0.4443        | freeze glove, B=16 | 8          |
+| `exp50`     | 0.445         | freeze glove, B=16 | 16         |
+| `exp50L`    | 0.4371        | train glove, B=16  | 4          |
+| `exp101`    | 0.4452        | freeze glove, B=16 | 16         |
+
+---
+
+| Experiment | Test Accuracy | Remark | Best Epoch |
+| ---------- | ------------- | ------ | ---------- |
--- config.py
+++ config.py
@@ -9,9 +9,9 @@ default_configs = dict(
     exp_name="default",
     stream_log_only=False,
     mode="train",
-    train_ratio=0.8,
+    train_ratio=0.9,
     val_flag=True,  # whether split the training data into validation set
-    batch_size=16,
+    batch_size=64,
     optimizer="Adam",
     learning_rate=0.0001,
     max_epochs=30,
@@ -23,22 +23,22 @@ default_configs = dict(
     task="classification",  # classification or regression
     json_dir="/home/mark/Data/NFT_Dataset/json",  # label json directory
     text_pickle="data/encoded_text.pickle",  # preprocessed text pickle file path
-    image_feat_h5="data/image_feats.h5",  # preprocessed image feature h5 file path
+    image_feat_h5="data/image_feats_resnet34_512.h5",  # preprocessed image feature h5 file path
     video_feat_h5="data/video_feats.h5",  # preprocessed video feature h5 file path
     audio_feat_h5="",  # preprocessed audio feature h5 file path
     # Model related
     word_dim=300,  # word embedding dimension
-    text_rnn_dim=512,  # image feature dimension
-    visual_in_dim=1000,  # image feature dimension
+    text_rnn_dim=256,  # image feature dimension
+    visual_in_dim=512,  # image feature dimension
     motion_in_frames=16,  # number of frames sampled per video
     motion_in_dim=512,  # video feature dimension
-    motion_mid_dim=256,  # video feature middle representation dimension
+    motion_mid_dim=128,  # video feature middle representation dimension
     agg_in_dim=256,  # feature aggregation input dimension
     agg_mid_dim=256,  # feature aggregation middle representation dimension
     agg_out_dim=256,  # feature aggregation output dimension
     num_classes=10,  # number of classes for classification task
     # misc
-    seed=0,  # seed for random number generator
+    seed=666,  # seed for random number generator
     gpu_ids="0",  # specify single or multiple GPU id to use
     num_workers=0,  # num of workers to use for DataLoader, 0 means no limit
     use_all_gpus=False,  # whether to use all gpus
@@ -89,9 +89,9 @@ class ExpConfigs:
     @staticmethod
     def load(path):
         with open(path, "r") as f:
-            return ExpConfigs(**eval(f.read()))
+            return ExpConfigs(**json.loads(f.read()))
 
 
 if __name__ == "__main__":
     config = ExpConfigs()
-    config.save("configs/template.json")
+    config.save("cfgs/template.json")
